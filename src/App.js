138c403aea4a6f6dd4fe3ae7eb8ddd6626389dff
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-10 23:51:09 +0400

    [feat] Initial squeleton : navbar + body of pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,84 @@
 import React from "react";
-import { withAuthenticator, AmplifyTheme } from "aws-amplify-react";
+import { Auth, Hub } from "aws-amplify";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import DoneListPage from "./pages/DoneListPage";
+import DonePage from "./pages/DonePage";
+import Navbar from "./components/Navbar";
 import "./App.css";
+import { render } from "@testing-library/react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    console.dir(AmplifyTheme);
+    this.getUserData();
+    Hub.listen("auth", this, "onHubCapsule");
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({ user }) : this.setState({ user: null });
+  };
+
+  onHubCapsule = (capsule) => {
+    console.log("capsule called");
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("signed in");
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log("signed up");
+        break;
+      case "signOut":
+        console.log("signed out");
+        this.setState({ user: null });
+        break;
+      default:
+        return;
+    }
+  };
+
+  handleSignout = async () => {
+    try {
+      await Auth.signOut();
+    } catch (err) {
+      console.error("Error during sign out", err);
+    }
+  };
+
+  render() {
+    const { user } = this.state;
+    return !user ? (
+      <Authenticator theme={theme}></Authenticator>
+    ) : (
+      <Router>
+        <>
+          {/* Navigation */}
+          <Navbar user={user} handleSignout={this.handleSignout} />
+          {/* Routes */}
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route exact path="/doneList" component={DoneListPage} />
+            <Route
+              exact
+              path="/done/:doneId"
+              component={({ match }) => (
+                <DonePage doneId={match.params.doneId} />
+              )}
+            />
+          </div>
+        </>
+      </Router>
+    );
+  }
 }
 
 const theme = {
@@ -26,4 +101,5 @@ const theme = {
   },
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+export default App;
+//export default withAuthenticator(App, true, [], null, theme);
