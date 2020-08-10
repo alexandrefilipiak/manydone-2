dbe9f95e6ddf24c730966d78449e6d2385b84222
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-10 23:50:32 +0400

    [feat] initial navbar version

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Menu as Nav, Icon, Button } from "element-react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = ({ user, handleSignout }) => (
+  <Nav>
+    <div className="nav-container">
+      {/* App title / Icon */}
+      <Nav.Item index="1">
+        <NavLink to="/" className="nav-Link">
+          <span className="app-title">
+            <img src="gloryroad-logo.png" alt="App-Icon" className="app-icon" />
+            Glory Road
+          </span>
+        </NavLink>
+      </Nav.Item>
+      <div className="nav-items">
+        <Nav.Item index="2">
+          <span className="app-user">Hello, {user.username}</span>
+        </Nav.Item>
+        <Nav-Item index="3">
+          <NavLink to="profile" className="nav-link">
+            <Icon name="setting" />
+            Profile
+          </NavLink>
+        </Nav-Item>
+        <Nav.Item index="4">
+          <Button type="warning" onClick={handleSignout}>
+            Sign out
+          </Button>
+        </Nav.Item>
+      </div>
+      {/* Navbar Items */}
+    </div>
+  </Nav>
+);
+
+export default Navbar;
