f44383990581b5dc76a1bf459f2150432f49d721
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-11 23:25:56 +0400

    [feat] New component for inputing a new done

---
+++ src/components/NewDone.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { createDone } from "../graphql/mutations";
+
+// prettier-ignore
+import { Form, Button, Dialog, Input, Select, Notification } from 'element-react'
+
+export default class NewDone extends Component {
+  state = {
+    name: "",
+    addDoneDialog: false,
+  };
+
+  handleAddDone = async () => {
+    try {
+      console.log(this.state.name);
+      this.setState({ addDoneDialog: false });
+      const input = {
+        name: this.state.naem,
+      };
+      const result = await API.graphql(graphqlOperation(createDone, { input }));
+      console.log(`Created Done: id ${result.data.createDone.id}`);
+      this.setState({ name: "" });
+    } catch (err) {
+      console.error(err);
+      Notification.error({
+        title: "Error",
+        message: `${err.message || "Error adding done"}`,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div className="market-header">
+          <h1 className="market-title">
+            Create your done
+            <Button
+              type="text"
+              icon="edit"
+              className="market-title-button"
+              onClick={() => this.setState({ addDoneDialog: true })}
+            />
+          </h1>
+        </div>
+
+        <Dialog
+          title="Create new done"
+          visible={this.state.addDoneDialog}
+          onCancel={() => this.setState({ addDoneDialog: false })}
+          size="large"
+          customClass="dialog"
+        >
+          <Dialog.Body>
+            <Form labelPosition="top">
+              <Form.Item label="Add Done Name">
+                <Input
+                  placeholder="Done Name"
+                  trim={true}
+                  onChange={(name) => this.setState({ name })}
+                  value={this.state.name}
+                />
+              </Form.Item>
+            </Form>
+          </Dialog.Body>
+          <Dialog.Footer>
+            <Button onClick={() => this.setState({ addDoneDialog: false })}>
+              Cancel
+            </Button>
+            <Button
+              type="primary"
+              disabled={!this.state.name}
+              onClick={this.handleAddDone}
+            >
+              Add
+            </Button>
+          </Dialog.Footer>
+        </Dialog>
+      </>
+    );
+  }
+}
