4ce5c7642141efd82a442c73de1acc7752ce67b8
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-15 09:54:35 +0400

    [feat] display list of Dones

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -1,7 +1,70 @@
 import React, { Component } from "react";
+import { graphqlOperation } from "aws-amplify";
+import { Connect } from "aws-amplify-react";
+import { listDones } from "../graphql/queries";
+import Error from "./Error";
+import { Loading, Card, Icon, Tag } from "element-react";
+import { Link } from "react-router-dom";
 
-export default class DoneList extends Component {
-  render() {
-    return <div>DoneList</div>;
-  }
-}
+const DoneList = () => {
+  return (
+    <>
+      <h2 className="header">
+        <img
+          src="https://icon.now.sh/store_mall_directory/527FFF"
+          alt="Store Icon"
+          className="large-icon"
+        />
+        DoneList
+      </h2>
+      <Connect query={graphqlOperation(listDones)}>
+        {({ data, loading, errors }) => {
+          if (errors.length > 0) return <Error errors={errors} />;
+          if (loading || !data.listDones) return <Loading fullscreen={true} />;
+
+          return (
+            <>
+              {data.listDones.items.map((done) => (
+                <div key={done.id} className="my-2">
+                  <Card
+                    bodyStyle={{
+                      padding: "0.7em",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <span className="flex">
+                      <Link className="link" to={`/done/${done.id}`}>
+                        {done.name}
+                      </Link>
+                      <span style={{ color: "var(--darkAmazonOrange)" }}>
+                        {done.description}
+                      </span>
+                      <img
+                        src="https://icon.now.sh/shopping_cart/f60"
+                        alt="Shopping cart"
+                      />
+                    </span>
+                    <div style={{ color: "var(--lightSquidInk" }}>
+                      {done.owner}
+                    </div>
+                    <div>
+                      {done.category && (
+                        <Tag type="danger" className="mx-1">
+                          {done.category}
+                        </Tag>
+                      )}
+                    </div>
+                  </Card>
+                </div>
+              ))}
+            </>
+          );
+        }}
+      </Connect>
+    </>
+  );
+};
+
+export default DoneList;
