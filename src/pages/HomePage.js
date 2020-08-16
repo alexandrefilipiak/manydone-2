12248f839479239b16666773cbc16dc91ec5a2af
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-16 08:43:45 +0400

    [feat] Added link to list of dones

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class HomePage extends Component {
   render() {
-    return <div>HomePage</div>;
+    return (
+      <div>
+        HomePage. Here is the link to the
+        <Link to="/doneList">Done List</Link>
+      </div>
+    );
   }
 }
