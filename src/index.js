cf6f0326e3341a1e84211b0b71fc16865d84fee0
Alexandre Filipiak <a.filipiak@gmail.com>
2020-08-18 08:48:33 +0400

    [feat] disabled strict mode

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import * as serviceWorker from "./serviceWorker";
 Amplify.configure(aws_exports);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>
   document.getElementById("root")
 );
 
